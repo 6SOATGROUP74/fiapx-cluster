19a126f1db988d18dc2ce324be4229a18016a631
igusvs <igoralves159@gmail.com>
2025-02-09 01:21:18 -0300

    ci/cd

--- src/local.tf
+++ src/local.tf
@@ -2,5 +2,5 @@ locals {
   cluster_name = "fiapx-cluster-eks"
   region       = "us-east-1"
   lab_role     = "arn:aws:iam::014732159800:role/LabRole"
-  subnets      = ["subnet-00ccec3e541e14af3", "subnet-007733da9c689a242"]
+  subnets      = ["subnet-0392bdb74c77a28c3", "subnet-04f5803374ec241fa"]
 }
